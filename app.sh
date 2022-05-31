7a5c939bfe6c645301fb2245148c83bd33872b83
IJECHRIS <105609091+IJECHRIS@users.noreply.github.com>
2022-05-31 10:43:01 -0400

    new feature automated

--- app.sh
+++ app.sh
@@ -3,3 +3,5 @@ echo "This is the Lord's doing and it is marvellous in my eyes"
 echo "Glory be to God".
 echo "Amen."
 #bugs removed
+car feature automated
+echo " hello $user, your car due for routine maintainance services"
